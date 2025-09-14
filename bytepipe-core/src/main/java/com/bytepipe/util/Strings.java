8a54ad94841a913fa44859c5d865aed6a1931eb6
Parag Paralikar <paragparalikar@gmail.com>
2025-09-14 16:12:14 +0530

    Utility class

---
+++ bytepipe-core/src/main/java/com/bytepipe/util/Strings.java
@@ -0,0 +1,14 @@
+package com.bytepipe.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Strings {
+
+    public String sanitize(String text){
+        return null == text ? null : text
+                .replace('\r', ' ')
+                .replace('\n', ' ');
+    }
+
+}
