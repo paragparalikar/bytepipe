4720cd9f326f419de4aff975d91b59ac580a3a89
Parag Paralikar <paragparalikar@gmail.com>
2025-09-23 19:29:54 +0530

    na

--- bytepipe-api/src/main/java/com/bytepipe/oracle/redolog/RedoLogStore.java
+++
@@ -1,4 +0,0 @@
-package com.bytepipe.oracle.redolog;
-
-public interface RedoLogStore {
-}
--- bytepipe-api/src/main/java/com/bytepipe/oracle/scn/ScnRange.java
+++ bytepipe-api/src/main/java/com/bytepipe/oracle/scn/ScnRange.java
@@ -1,37 +1,21 @@
 package com.bytepipe.oracle.scn;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@ToString(exclude = {"redoLogCount", "globalLogMinerParallelism"})
-@EqualsAndHashCode(exclude = {"redoLogCount", "globalLogMinerParallelism"})
-public class ScnRange implements Comparable<ScnRange>, Serializable {
 
-    private BigInteger startScn, endScn;
-    private volatile int redoLogCount, globalLogMinerParallelism;
+public record ScnRange(BigInteger startScn, BigInteger endScn) implements Comparable<ScnRange>, Serializable {
 
-    public ScnRange(BigInteger startScn, BigInteger endScn){
-        this.endScn = endScn;
-        this.startScn = startScn;
-    }
-
-    public boolean contains(ScnRange other){
+    public boolean contains(ScnRange other) {
         return null != other
                 && startScn.compareTo(other.startScn) <= 0
                 && endScn.compareTo(other.endScn) >= 0;
     }
 
     @Override
-    public int compareTo(ScnRange other){
+    public int compareTo(ScnRange other) {
         return Objects.compare(
                 null == startScn ? BigInteger.ZERO : startScn,
                 null == other || null == other.startScn ? BigInteger.ZERO : other.startScn,
